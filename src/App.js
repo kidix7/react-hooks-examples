0924efaad137f52e5b3105acd56a27c19f1a1502
Diogo Neves <diogo.neves@tmceurope.com>
2019-04-03 14:10:37 +0100

    Next step -> Add Payment.jsx with a list of people

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Fragment } from "react";
 import "./App.css";
 import _ from 'lodash';
 import InputPersons from "./InputPersons";
+import Payment from "./Payment";
 
 export default () => {
 
   return (
     <Fragment>
         <InputPersons />
+        <Payment />
     </Fragment>
   );
 };
--- src/Payment.jsx
+++ src/Payment.jsx
@@ -1,8 +1,14 @@
 import React, { useState , Fragment} from "react";
-import  map  from 'lodash/map';
+import { map } from 'lodash';
 
-function Payment ({ onSubmit }, people) {
+export default (people) => {
    
-};
+  return <>
+          { map(people, (val) => 
+            <>
+              <input type='checkbox' key={val} /> {val} 
+            </>
+          )}
+      </>;
 
-export default Payment;
+};
